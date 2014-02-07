fe13b60e8f10099abe2dc571e1a1a91172bf2313
keeronmarc <keeronmarc@gmail.com>
2014-02-06 21:40:01 -0500

    Changes to javascript files

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,22 @@
-console.log('\'Allo \'Allo!');
+var youTubeURL = 'http://gdata.youtube.com/feeds/api/videos/pco91kroVgQ?v=2&alt=json';
+
+            var json = (function() {
+                var json = null;
+                $.ajax({
+                    'async': false,
+                    'global': false,
+                    'url': youTubeURL,
+                    'dataType': "json",
+                    'success': function(data) {
+                        json = data;
+                    }
+                });
+                return json;
+            })();
+            
+            var str = json.entry.yt$statistics.viewCount;
+            if(str.length > 10) str = str.substring(0,10);
+
+            $('.uniquely').append(str);
+
+
